b9c0cc9a19e792d867bf866ea3e778f17febfd37
paulneo <jpaulneo2k@gmail.com>
2018-05-14 22:37:03 -0500

    ddd

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,18 @@
 class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+
+  end
+  def search_address_maps
+    @name = "Alexei"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students
+  get "search_address_maps", to: "students#search_address_maps"
+
 end
