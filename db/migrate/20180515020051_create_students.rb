a587102959859d1d04f8221144d4ae84d1cc5031
paulneo <jpaulneo2k@gmail.com>
2018-05-14 21:20:27 -0500

    add model and cotrolador

---
+++ db/migrate/20180515020051_create_students.rb
@@ -0,0 +1,12 @@
+class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :last_name
+      t.string :address
+      t.string :code
+
+      t.timestamps
+    end
+  end
+end
