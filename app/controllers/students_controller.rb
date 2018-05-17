30f1ced3f5241d7e1bd51ccd99c98eba0edf5ad3
paulneo <jpaulneo2k@gmail.com>
2018-05-16 21:18:47 -0500

    add

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,6 +21,24 @@ class StudentsController < ApplicationController
     end
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+
+    if @student.update student_params
+      redirect_to @student
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to students_path
+  end
   private
 
   def student_params
