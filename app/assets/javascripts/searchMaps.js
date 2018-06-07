bb3d36de6be0d072e47327605444913233dc4892
paulneo <jpaulneo2k@gmail.com>
2018-06-07 18:05:39 -0500

    add

--- app/assets/javascripts/searchMaps.js
+++ app/assets/javascripts/searchMaps.js
@@ -1,67 +1,75 @@
 
-function initAutocomplete() {
-  var map2 = new google.maps.Map(document.getElementById('map2'), {
-    center: {lat:-15.4954666, lng:-70.1416507},
-    zoom: 13,
-    mapTypeId: 'roadmap'
-  });
 
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map2.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map2.addListener('bounds_changed', function() {
-    searchBox.setBounds(map2.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
+  function initAutocomplete() {
+    let map2 = new google.maps.Map(document.getElementById('map2'), {
+      center: {lat:-15.4954666, lng:-70.1416507},
+      zoom: 13,
+      mapTypeId: 'roadmap'
     });
-    markers = [];
 
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
+    let input = document.getElementById('pac-input');
+    let pacInputComplete = document.getElementById('pac-input-complete');
+
+
+    let searchBox = new google.maps.places.SearchBox(input);
+    map2.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+
+
+    input.addEventListener('change', ()=>{
+      let val = input.value
+      pacInputComplete.value = val
+    })
+
+    map2.addListener('bounds_changed', function() {
+      searchBox.setBounds(map2.getBounds());
+    });
+
+    let markers = [];
+
+    searchBox.addListener('places_changed', function() {
+      let places = searchBox.getPlaces();
+
+      if (places.length == 0) {
         return;
       }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
 
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map2,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
 
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
+
+      let bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+
+          return;
+        }
+        let icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+
+        markers.push(new google.maps.Marker({
+          map: map2,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map2.fitBounds(bounds);
     });
-    map2.fitBounds(bounds);
-  });
-}
+
+
+  }
